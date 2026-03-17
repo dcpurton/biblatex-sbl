950725db0aaec23dfbdfdc4f2a72251d2970d6b5
David Purton <dcpurton@marshwiggle.net>
2026-03-17 23:31:56 +1030

    Begin main documentation (with version tagging)

--- build.lua
+++ build.lua
@@ -3,6 +3,7 @@ module = "biblatex-sbl"
 unpackexe        = ""
 sourcefiles      = {"*.def", "*.bib", "*.cbx", "*.bbx", "*.dbx", "*.lbx"}
 installfiles     = {"*.def", "*.cbx", "*.bbx", "*.dbx", "*.lbx"}
+typesetfiles     = {"biblatex-sbl.tex"}
 typesetdemofiles = {"biblatex-sbl-blog-examples.tex",
                     "biblatex-sbl-studentsupplement-examples.tex",
                     "biblatex-sbl-handbook-examples.tex"}
@@ -22,3 +23,23 @@ function runtest_tasks (name, run)
   end
 end
 
+tagfiles = {"biblatex-sbl.tex", "biblatex-sbl.def"}
+
+function update_tag(file, content, tagname, tagdate)
+  content = content:gsub(
+    "(\\date%{)%d%d%d%d%-%d%d%-%d%d%s+v[%d%.%a]+(%})",
+    "%1" .. tagdate .. " v" .. tagname .. "%2"
+  )
+
+  content = content:gsub(
+    "(\\def\\sbl@abx@date%{)%d%d%d%d%-%d%d%-%d%d(%})",
+    "%1" .. tagdate .. "%2"
+  )
+
+  content = content:gsub(
+    "(\\def\\sbl@abx@version%{)[%d%.%a]+(%})",
+    "%1" .. tagname .. "%2"
+  )
+
+  return content
+end
